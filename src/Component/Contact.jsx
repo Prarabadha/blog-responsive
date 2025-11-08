15a7aa767bd1cecfb65eb1baccfebf23624382c3
Prarabdha <prarabadhabhagat@gmail.com>
2025-11-08 12:01:14 +0530

    added the contact page

--- src/Component/Contact.jsx
+++ src/Component/Contact.jsx
@@ -1,6 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function Contact() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+
+  const [error, setError] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // Check the Error logic
+    const errorCheck = {
+      name:
+        name.trim().length < 2 ? "Name must be grater than 2 character" : "",
+      email: /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email) ? "" : "Invalid email",
+      message: message.length < 20 ? "password must be 20 character" : "",
+    };
+
+    setError(errorCheck);
+
+    const isValid = Object.values(errorCheck).every((err) => err === "");
+
+    if (isValid) {
+      alert(
+        `Form submitted succesfully : Name:${name} | Email:${email} | message:${message}`
+      );
+
+      setName("");
+      setEmail("");
+      setMessage("");
+    }
+  };
+
   return (
     <section className="bg-[#0A0F2C] text-white py-12 px-4">
       <div className="max-w-[1200px] mx-auto flex flex-col md:flex-row items-center justify-between  rounded-2xl p-8 md:p-12">
@@ -24,19 +60,25 @@ export default function Contact() {
               type="text"
               placeholder="Your Name*"
               className="p-3 rounded-md outline-none text-gray-900"
+              onChange={(e) => setName(e.target.value)}
+              value={name}
             />
             <input
               type="email"
               placeholder="Your Email*"
               className="p-3 rounded-md outline-none text-gray-900"
+              onChange={(e) => setEmail(e.target.value)}
+              value={email}
             />
             <textarea
               placeholder="Your Message*"
               rows="4"
               className="p-3 rounded-md outline-none text-gray-900 resize-none"
+              onChange={(e) => setMessage(e.target.value)}
+              value={message}
             />
             <button
-              type="submit"
+              onClick={handleSubmit}
               className="bg-gradient-to-r from-blue-600 to-indigo-600 hover:from-blue-700 hover:to-indigo-700 text-white font-semibold py-2 px-4 rounded-md transition duration-300 w-fit"
             >
               Send Message
